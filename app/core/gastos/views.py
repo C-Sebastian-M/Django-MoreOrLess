82279d77966f225144c525c6085fa06a28e3bf4a
Oscar-Barrero <oscardanielbr@gmail.com>
2021-08-09 16:45:27 -0500

    Organice y agregue herencia en listas, apps y urls

--- app/core/gastos/views.py
+++ app/core/gastos/views.py
@@ -10,5 +10,6 @@ class GastosListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['title'] = 'Gastos registrados'
         #context['index_url'] = reverse_lazy('index')
         return context
